ee38d9dd2a81e38673ffdc0d50313310fef9470d
jungheeeun <jungheeeun272@gmail.com>
2023-12-18 14:37:25 +0900

    Footer 위젯 : StikerModel을 stikers 변수에 입력하기

--- image_editor1/lib/screen/home_screen.dart
+++ image_editor1/lib/screen/home_screen.dart
@@ -3,6 +3,9 @@ import 'package:image_editor1/component/main_app_bar.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io'; // 파일 관련 기능을 사용하기 위해 추가
 import 'package:image_editor1/component/footer.dart';
+import 'package:image_editor1/model/sitcker_model.dart';
+import 'package:image_editor1/component/emotion_sticker.dart';
+import 'package:uuid/uuid.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -14,6 +17,9 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
 
   XFile? image; // 선택한 이미지를 저장할 변수
+  Set<StickerModel> stikers = {}; // 화면에 추가된 스티커를 저장할 변수
+  String? selectedId; // 현재 선택된 스티커의 ID
+
 
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,14 +58,27 @@ class _HomeScreenState extends State<HomeScreen> {
     if (image != null) {
       // Stack 크기의 최대 크기만큼 차지
       return Positioned.fill(
-
         // 위젯 확대 및 좌우 이동을 가능하게 하는 위젯
         child: InteractiveViewer(
-          child: Image.file(
+          child: Stack(
+            fit: StackFit.expand, // 크기 최대로 늘려주기
+            children: [
+            Image.file(
             File(image!.path),
-
             // 이미지가 부모 위젯 크기의 최대를 차지하도록 하기
             fit: BoxFit.cover,
+            ),
+              ...stikers.map(
+                  (stiker) => Center( // 최초 스티커 선택시 중앙에 배치
+                 child: EmoticonStictker(,
+                    key: ObjectKey(stiker.id),
+                    onTransform: onTransform,
+                    imgPath : stiker.imgPath,
+                    isSelected : selectedId == stiker.id,
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       );
@@ -90,5 +109,17 @@ class _HomeScreenState extends State<HomeScreen> {
 
   void onDeleteItem() {}
 
-  void onEmoticonTap(int index){}
+  void onEmoticonTap(int index) async{
+    setState(() {
+      stikers = {
+        ...stikers,
+        StickerModel(
+          id: Uuid().v4(), // 스티커의 고유 ID
+          imgPath: 'asset/img/emoticon_$index.png',
+        ),
+      };
+    });
+  }
+
+  void onTransform() {}
 }
